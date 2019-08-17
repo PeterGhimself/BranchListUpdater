f4ddaee3b7d1d2b040905af6208ce57c7434385e
PeterGhimself <petergranitski@gmail.com>
2019-08-17 17:22:51 -0400

    Script now appends first arg (or default value) to list of branches and saves new list

--- branch_list_updater.py
+++ branch_list_updater.py
@@ -3,6 +3,15 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import getpass
+import re
+import time
+import sys
+
+# set default name if none passed in args
+new_branch_name = 'testy'
+
+if len(sys.argv) > 1:
+    new_branch_name = sys.argv[1]
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
 chromedriver_path = current_dir + 'chromedriver'
@@ -15,19 +24,7 @@ chrome_options.add_argument("--no=sandbox") # required when running as root user
 
 driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=chrome_options, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 
-# Option 2 - with pyvirtualdisplay
-'''
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(1024, 768))
-display.start()
-driver = webdriver.Chrome(driver_path=chromedriver_path, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
-'''
-# Log path added via service args to see errors if something goes wrong
-
-# And now you can add your website / app testing functionality:
-
-#target_url = 'https://spaceconcordiateam.slack.com'
-# directly to github enterprise server integration settings page, which will first redirect to login page
+# directly go to github enterprise server integration settings page, which will first redirect to login page
 # after succesful login we should hit the 'Github Enterprise Server | Slack App Directory' page
 target_url = 'https://spaceconcordiateam.slack.com/services/B2ZGUM4MV'
 
@@ -36,13 +33,13 @@ print('driver.title: ', driver.title)
 
 signed_in = False
 
-# check if already logged in
+# just in case, check if already logged in
 try:
     elem = driver.find_element_by_class_name('p-classic_nav__team_header__user__name__truncate')
     print('elem: ', elem)
 
 except Exception as err:
-    print('Error encountered!')
+    print('Currently not signed in')
     print(err)
 
 email = ''
@@ -55,7 +52,7 @@ if not signed_in:
         password = driver.find_element_by_id('password')
         print('password: ', password)
 
-    except NoSuchElementException as err:
+    except Exception as err:
         print('Error encountered!')
         print(err)
 
@@ -76,10 +73,43 @@ elem = ''
 
 # check to see if login worked
 try:
-    assert driver.page_source.find('GitHub Enterprise Server')
     print('driver.title: ', driver.title)
 
-except NoSuchElementException as err:
+    '''
+    # One way of checking the page source for matches
+    src = driver.page_source
+    text_found = re.search(r'class="branches small"', src)
+    print('text_found', text_found)
+    '''
+
+    # append branch name to branch list
+    print('locating branches')
+
+    # this method doesn't seem to be working
+    #branch_list = driver.find_element_by_class_name('branches')
+    #branch_list.send_keys(', test!')
+
+    # so let's try javscripting this instead
+    print('inserting value: ', new_branch_name)
+    branches_val = "document.getElementsByName('branches[]')[1].value"
+    set_branches = branches_val + " += ', " + new_branch_name + "'"
+    print('set_branches', set_branches)
+    get_branches = "return " + branches_val
+    driver.execute_script(set_branches)
+
+    # visual confirmation
+    print('updated branch list: ', driver.execute_script(get_branches))
+
+    # necessary wait, otherwise save button gets clicked too fast to actually work
+    time.sleep(0.5)
+
+    # only parent is clickable with selenium driver
+    # although in js child is clickable
+    print('locating save button')
+    save = driver.find_element_by_id('add_integration_parent')
+    save.click()
+
+except Exception as err:
     print('Error encountered!')
     print(err)
 
