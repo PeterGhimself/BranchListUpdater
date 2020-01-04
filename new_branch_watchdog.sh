82336efd168f1b6d98180f85d10acad82c9527d4
PeterGhimself <petergranitski@gmail.com>
2020-01-03 23:39:32 -0500

    Watchdog script supports multiple branch names and passes them to branch_list_updater.py

--- new_branch_watchdog.sh
+++ new_branch_watchdog.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
 
 new_branch_rgx="[new branch]"
-PATH_TO_UPDATER="/home/$USER/Programming/fun/BranchListUpdater/branch_list_updater.py"
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+PATH_TO_UPDATER="$SCRIPT_PATH/branch_list_updater.py"
+NEW_BRANCHES="$SCRIPT_PATH/new_branches.txt"
+LAST_PULL_LOG="SCRIPT_PATH/last_pull_result.log"
+
+function join_by {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
 
 while :
 do
   # loop infinitely
   pull_result=$(git pull 2>&1)
   echo "$pull_result"
-  echo "$pull_result" > example.txt
+  echo "$pull_result" > $LAST_PULL_LOG
 
   if [[ $pull_result == *"$new_branch_rgx"* ]]; then
-    #@TODO: handle multiple branch names
-
+    # initial parse
     new_branch_dirty=$(echo "$pull_result" | grep -o -P '(?<= ).*(?= ->)')
 
-    echo "new_branch_dirty: $new_branch_dirty" > new_branch.txt
+    # temp storage
+    echo "$new_branch_dirty" > $NEW_BRANCHES
 
     # remove trailing whitespace
-    sed -i '' -e's/[ \t]*$//' "new_branch.txt"
+    sed -i '' -e's/[ \t]*$//' $NEW_BRANCHES
     # get just the last word
-    sed -i 's/.* //' "new_branch.txt"
-    new_branch=`cat new_branch.txt`
-    echo "New branch detected: $new_branch"
-    echo "Running branch_list_updater.py with arg: $new_branch"
-    python $PATH_TO_UPDATER $new_branch
+    sed -i 's/.* //' $NEW_BRANCHES
+
+    # handle multiple branch names (as they appear line by line)
+    branches=()
+    while read branch;
+      do branches+=($branch);
+    done < $NEW_BRANCHES
+    # join by delimiter ',' as branch_list_updater.py expects it
+    branches=`join_by , ${branches[@]}`
+
+    echo "New branches detected: $branches"
+    echo "Running branch_list_updater.py with arg: $branches"
+    python $PATH_TO_UPDATER "$branches"
   else
     echo "No new branches detected"
   fi
