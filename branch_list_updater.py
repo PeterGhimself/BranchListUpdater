a16c50a1f4d8f4ae51b2d237f83faf0f801bcc8f
PeterGhimself <petergranitski@gmail.com>
2020-02-02 02:23:26 -0500

    Make use of logging module for blu
    Hopefully this will make it easier to debug if something weird happens

--- branch_list_updater.py
+++ branch_list_updater.py
@@ -8,6 +8,17 @@ import time
 import sys
 import traceback
 import slackconfig as cfg
+import logging
+from datetime import datetime
+
+# branch list updater output log file
+OUTPUT_LOG = 'blu_out.log'
+
+# setup logger so we can simultaneously print and log to file
+level = logging.INFO
+format = '  %(message)s'
+handlers = [logging.FileHandler(OUTPUT_LOG), logging.StreamHandler()]
+logging.basicConfig(level = level, format = format, handlers = handlers)
 
 # set default name if none passed in args
 new_branch_name = 'testy'
@@ -36,37 +47,40 @@ BRANCH_LIST_FILE = 'updated_branch_list'
 #@TODO: add usage string
 #@TODO: search and replace (given an empty string to replace with, delete that entry)
 
+start_ts = datetime.now()
+logging.info('starting blu job at: ' + str(start_ts))
+
 if len(sys.argv) > 1:
     new_branch_name = sys.argv[1]
 
     if new_branch_name in LIST_FLAGS:
         list_branches = True
-        print(LIST_MODE_MSG)
+        logging.info(LIST_MODE_MSG)
     # try to catch typos
     elif 'list' in new_branch_name:
         answer = input(LIST_FLAG_TYPO_MSG + '\n' + EXIT_PROMPT + USER_INPUT_OPTIONS)
         if answer.lower() == 'y':
             list_branches = True
-            print(LIST_MODE_MSG)
+            loggin.info(LIST_MODE_MSG)
         elif answer.lower() == 'q':
-            print(EXIT_OPTION_MSG)
+            logging.info(EXIT_OPTION_MSG)
             sys.exit(0)
         else:
-            print(UPDATE_MODE_MSG)
+            logging.info(UPDATE_MODE_MSG)
     else:
         # check if multiple branch names
         if MULTI_ARG_DELIMETER in new_branch_name:
-            print('Multiple branch names detected')
+            logging.info('Multiple branch names detected')
             new_branch_names = new_branch_name.split(MULTI_ARG_DELIMETER)
             # remove duplicates
             new_branch_names = list(dict.fromkeys(new_branch_names))
 
             for branch in new_branch_names:
-                print("Found: " + branch)
+                logging.info("Found: " + branch)
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
 chromedriver_path = current_dir + 'chromedriver'
-print('chromedriver_path: ', chromedriver_path)
+logging.info('chromedriver_path: ' +  str(chromedriver_path))
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
@@ -80,7 +94,7 @@ driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=chro
 target_url = 'https://spaceconcordiateam.slack.com/services/B2ZGUM4MV'
 
 driver.get(target_url)
-print('Page title: ', driver.title)
+logging.info('Page title: ' + str(driver.title))
 
 email = ''
 password = ''
@@ -91,17 +105,17 @@ try:
     password = driver.find_element_by_id('password')
 
 except Exception as err:
-    print('Error encountered!')
-    print(err)
-    traceback.print_exc()
+    logging.info('Error encountered!')
+    logging.info(err)
+    logging.info(traceback.print_exc())
 
 # check config file
 if cfg.user['email'] and cfg.user['password']:
-    print('Using user config set in slackconfig.py')
+    logging.info('Using user config set in slackconfig.py')
     user_email = cfg.user['email']
     user_password = cfg.user['password']
 
-    print('Using email:', user_email)
+    logging.info('Using email: ' + str(user_email))
 
     email.clear()
     email.send_keys(user_email)
@@ -110,8 +124,8 @@ if cfg.user['email'] and cfg.user['password']:
     password.send_keys(Keys.RETURN)
 
 else:
-    print('Config file either missing user, password, or both')
-    print('Enter email: ')
+    logging.info('Config file either missing user, password, or both')
+    logging.info('Enter email: ')
     user_email = input()
 
     email.clear()
@@ -127,12 +141,12 @@ elem = ''
 
 # check to see if login worked
 try:
-    print('Login success, on page: ', driver.title)
+    logging.info('Login success, on page: ' + str(driver.title))
 
     if 'GitHub Enterprise Server | Slack App Directory' not in driver.title:
-        print('Sign in failed, closing driver...')
+        logging.info('Sign in failed, closing driver...')
         driver.close()
-        print('exiting script')
+        logging.info('exiting script')
         sys.exit()
 
     '''
@@ -143,14 +157,14 @@ try:
     '''
 
     # append branch name to branch list
-    print('locating branches...')
+    logging.info('locating branches...')
 
     # this method doesn't seem to be working
     #branch_list = driver.find_element_by_class_name('branches')
     #branch_list.send_keys(', test!')
 
     # so let's try javscripting this instead
-    print('inserting value: ', new_branch_name)
+    logging.info('inserting value: ' + str(new_branch_name))
     #TODO: try to implement it properly as shown in 'get_branch_list.js' vs this hack
     branches_val = "document.getElementsByName('branches[]')[2].value"
 
@@ -158,8 +172,8 @@ try:
     if list_branches:
         get_branches = 'return ' + branches_val 
         branch_list = driver.execute_script(get_branches)
-        print('listing current branches:')
-        print(branch_list)
+        logging.info('listing current branches: ')
+        logging.info(branch_list)
 
         # save list to file
         with open(BRANCH_LIST_FILE, 'w+') as f:
@@ -185,13 +199,13 @@ try:
             set_branches += "'"
         else:
             set_branches = branches_val + " += ', " + new_branch_name + "'"
-        print('set_branches', set_branches)
+        logging.info('set_branches: ' + str(set_branches))
         get_branches = "return " + branches_val
         driver.execute_script(set_branches)
         updated_branch_list = driver.execute_script(get_branches)
 
         # visual confirmation
-        print('updated branch list: ', updated_branch_list)
+        logging.info('updated branch list: ' + str(updated_branch_list))
 
         # save list to file
         with open(BRANCH_LIST_FILE, 'w+') as f:
@@ -202,15 +216,20 @@ try:
 
         # only parent is clickable with selenium driver
         # although in js child is clickable
-        print('locating save button...')
+        logging.info('locating save button...')
         save = driver.find_element_by_id('add_integration_parent')
         save.click()
 
 except Exception as err:
-    print('Error encountered!')
-    print(err)
+    logging.info('Error encountered!')
+    logging.info(err)
     traceback.print_exc()
 
-print('closing driver...')
+logging.info('closing driver...')
 driver.close()
-print('driver closed')
+logging.info('driver closed')
+
+end_ts = datetime.now()
+logging.info('ending blu job at: ' + str(end_ts))
+
+
